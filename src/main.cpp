4034287a95fd096c1d99dd1bf396faf839333980
lee <lee.hudson1384@yahoo.co.uk>
2022-05-25 20:37:48 +0100

    Don't know

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 SoftwareSerial debugPort(6, 7);
 bool wokenUp;
+#define SERIAL_RX_BUFFER_SIZE 64
 
 #define AUTO_BAUD_CMD       "AT\r\n"
 #define TEXT_MODE_CMD       "AT+CMGF=1\r\n"
@@ -25,8 +26,79 @@ struct RESPONSE{
 MSG_CONTENTS message;
 RESPONSE response;
 
+String _readSerial(uint32_t timeout)
+{
+
+    uint64_t timeOld = millis();
 
 
+    while (!Serial.available() && !(millis() > timeOld + timeout))
+    {
+        delay(13);
+    }
+
+    String str;
+
+    while(Serial.available())
+    {
+        if (Serial.available()>0)
+        {
+            str += (char) Serial.read();
+        }
+    }
+
+    return str;
+
+}
+
+String sendCommandSimple(String cmd){
+  Serial.print(cmd);
+  delay(500);
+  return _readSerial(1000);
+}
+
+char* sendCommand(char *cmd){
+  char *buffer;
+  int lineCounter=0, charCounter=0,timeoutCounter=0,rxComplete=0;
+  char terminationChar = '\n';
+  int timeout = 10;
+
+  buffer = (char*)calloc(200,sizeof(char));
+  Serial.print(cmd);
+  delay(500);
+
+  while(rxComplete == 0){
+    while(Serial.available()){
+      timeoutCounter = 0;
+      buffer[charCounter] = Serial.read();
+      charCounter ++;
+    }
+    //debugPort.println(4);
+
+    timeoutCounter ++;
+    delay(50);
+    if(timeoutCounter >= timeout)
+      rxComplete = 1;
+  }
+  //debugPort.println(buffer);
+  return buffer;
+  /*
+  debugPort.print("Got ");
+  debugPort.print(lineCounter);
+  debugPort.println(" lines");
+
+  for(int j =0; j < lineCounter; j++){
+    debugPort.print("Line ");
+    debugPort.print(j);
+    debugPort.println(":");
+    debugPort.print(buffer[j]);
+  }
+
+    debugPort.println();
+    */
+}
+
+/*
 String sendCommand(String cmd){
 
   char buffer[256];
@@ -61,86 +133,58 @@ String sendCommand(String cmd){
     Serial.read();
   }
   return buffer;
-}
+}*/
 
 
-
-
-
-
-int getMostRecentMSGIndex(char inp[]){
-    char buf[4];
-    char catInt[4];
-    String rxString = String(inp);
-    rxString.toCharArray(buf,rxString.length());
-
-    if(isdigit(buf[rxString.length()-4])){
-        sprintf(catInt,"%c%c",buf[rxString.length()-4],buf[rxString.length()-3]);
-    }else{
-        sprintf(catInt,"%c",buf[rxString.length()-3]);
-    }
-
-    return atoi(catInt);
-}
-
-int checkForSms(){
-
-  char buffer[256];
-  char cmd[256];
-  int smsIndex =0;
-  MSG_CONTENTS message;
-  int availableBytes= Serial.available();
-
-  if(availableBytes>0){
-    debugPort.println("Got something");
-
-    for(int i=0; i<availableBytes; i++)
-    {
-        buffer[i] = Serial.read();
-    }
-    debugPort.print(buffer);
-
-    if(strstr(buffer,"+CMTI:")!=NULL){
-      debugPort.println("Found CMTI");
-      smsIndex=getMostRecentMSGIndex(buffer);
-      debugPort.println(smsIndex);
-      sprintf(cmd,"AT+CMGR=%d\r\n",smsIndex);
-      debugPort.print(sendCommand(cmd));
-      delay(5000);
-      debugPort.print(sendCommand(DELETE_MSGS_CMD));
-    }
-    else
-      smsIndex = -1;
+void printByte(char *buf){
+  //debugPort.println(strlen(buf));
+  for(int i = 0; i < strlen(buf); i++){
+    debugPort.write(buf[i]);
   }
-  return smsIndex;
 }
-
+int i;
 void setup() {
   // put your setup code here, to run once:
   debugPort.begin(57600);
   Serial.begin(57600);
   digitalWrite (2, HIGH);  // enable pull-up
   wokenUp = false;
-  
+  i =0;
   delay(1000);
   debugPort.print(sendCommand(AUTO_BAUD_CMD));
+  delay(1000);
   debugPort.print(sendCommand(TEXT_MODE_CMD));
+  delay(1000);/*
+  debugPort.print(sendCommand(CHECK_BATTERY_CMD));
+  delay(1000);
+  debugPort.print(sendCommand(AUTO_BAUD_CMD));
+  delay(1000);
+  debugPort.print(sendCommand("AT+CMGR=1\r\n"));
+  delay(1000);
+  debugPort.print(sendCommand(CHECK_BATTERY_CMD));
+  delay(1000);*/
+  //debugPort.print(sendCommand(AUTO_BAUD_CMD));
+  //debugPort.print(sendCommand(TEXT_MODE_CMD));
   /*
   debugPort.print(sendCommand(CHECK_BATTERY_CMD));
   debugPort.print(sendCommand(SLEEP_CMD));
   delay(1000);*/
-  debugPort.println("Ready");
+  //debugPort.println("Ready");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  debugPort.println(checkForSms());
+  //debugPort.println(checkForSms());
+  //delay(1000);
+  //debugPort.println(i);
+  //debugPort.print(sendCommand("AT+CMGR=1\r\n"));
+  //debugPort.flush();
+  i++;
   delay(1000);
-
-  /*
-  if (Serial.available()) {      // If anything comes in Serial (USB),	
+  /*  
+  if (Serial.available()) {      // If anything comes in Serial (USB);,	
     debugPort.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
 
   }
